18852d94984ceb7f4d9efc3e535228976f8f706d
Julian Lewandowski <julian.lewandowski365@gmail.com>
2025-02-22 13:07:22 +0000

    Added react app

---
+++ my-app/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import './App.css';
+import LandingPage from './pages/LandingPage'
+import Header from './components/Header';
+import Dashboard from './pages/Dashboard';
+import SubscriptionManager from './pages/SubscriptionManager';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/subscriptions" element={<SubscriptionManager />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
