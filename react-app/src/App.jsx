1b9dbd1198043b1823f343885d7d4be800174907
Julian Lewandowski <julian.lewandowski365@gmail.com>
2025-02-22 18:42:08 +0000

    .

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./index.css"; 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import LandingPage from "./pages/LandingPage";
